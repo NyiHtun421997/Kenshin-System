cb73caaefa14d3bfed688893da80813f5a18a198
NyiHtun421997 <133175373+NyiHtun421997@users.noreply.github.com>
2023-11-06 03:49:40 +0900

    Maven Structre

--- KenshinSystem/src/main/java/com/programming/nyihtuun/kenshin_desktop/CallBack.java
+++
@@ -1,9 +0,0 @@
-package main.java.com.programming.nyihtuun.kenshin_desktop;
-
-import javax.swing.JButton;
-
-//Obeserver Pattrn
-public interface CallBack {
-	void onButtonClicked(String componentText,JButton b);
-
-}
